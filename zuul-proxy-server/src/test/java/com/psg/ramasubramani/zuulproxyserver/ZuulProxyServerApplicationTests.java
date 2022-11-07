27397bfb2300757ce293b47e9b4e8239d32ac409
Ramasubramani N <rms.ece@gmail.com>
2022-11-07 23:53:17 +0530

    Adding Zuul Proxy as API Gateway

---
+++ zuul-proxy-server/src/test/java/com/psg/ramasubramani/zuulproxyserver/ZuulProxyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psg.ramasubramani.zuulproxyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulProxyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
